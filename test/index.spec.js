05760eb9248ea33d49244d16f0a535a7c8e0800f
J Huang <i@jhuang.me>
2017-01-01 17:37:24 +0800

    test(index): correct test code

--- test/index.spec.js
+++ test/index.spec.js
@@ -8,10 +8,10 @@ import PropsViewer from '../src'
 test('props-viewer should generate a component', (t) => {
   const Robot = PropsViewer('Robot')
 
-  t.plan(3)
-  t.ok(typeof Robot, 'function')
-  t.ok(Robot.length, 1)
-  t.ok(Robot.displayName, 'Robot')
+  t.equal(typeof Robot, 'function')
+  t.equal(Robot.length, 1)
+  t.equal(Robot.displayName, 'Robot')
+  t.end()
 })
 
 test('shallow', (t) => {
@@ -22,7 +22,7 @@ test('shallow', (t) => {
 
   const wrapper = shallow(<Robot {...props} />)
 
-  t.plan(2)
-  t.ok(wrapper.contains(<span>Robot</span>), true)
-  t.ok(wrapper.contains(<JSONTree data={props} />), true)
+  t.equal(wrapper.contains(<span>Robot</span>), true)
+  t.equal(wrapper.contains(<JSONTree data={props} />), true)
+  t.end()
 })
